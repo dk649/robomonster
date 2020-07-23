fe3234798f3649de2a290cb7e28cced71e261538
dk649 <58308212+dk649@users.noreply.github.com>
2020-07-23 16:39:58 +0100

    Updates

---
+++ precache-manifest.47434dbf1c21ef527e66df25542fd9c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6d418d3a4e47443568cbf5ecec40cf1",
+    "url": "/robomonster/index.html"
+  },
+  {
+    "revision": "059ac4fc70b0d4b9110f",
+    "url": "/robomonster/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "421e7f79c87696113c0b",
+    "url": "/robomonster/static/js/2.1ab49bb2.chunk.js"
+  },
+  {
+    "revision": "b3fa0e79498bf67a2879816b216b1b9d",
+    "url": "/robomonster/static/js/2.1ab49bb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059ac4fc70b0d4b9110f",
+    "url": "/robomonster/static/js/main.f8f938d0.chunk.js"
+  },
+  {
+    "revision": "9ef59d09d1b251400304",
+    "url": "/robomonster/static/js/runtime-main.d6f4af13.js"
+  }
+]);
